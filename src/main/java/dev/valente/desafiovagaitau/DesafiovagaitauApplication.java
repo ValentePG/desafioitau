51c85ac8ec3c30adf0253a448d6ce6c95a442e2b
Valente <bielvhv3@gmail.com>
2025-02-27 20:36:31 -0300

    Application

---
+++ src/main/java/dev/valente/desafiovagaitau/DesafiovagaitauApplication.java
@@ -0,0 +1,13 @@
+package dev.valente.desafiovagaitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiovagaitauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiovagaitauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/valente/desafiovagaitau/DesafiovagaitauApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.valente.desafiovagaitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiovagaitauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
