28be7faab1a113497b78a81d6e50c22c97a5b1a7
Valente <bielvhv3@gmail.com>
2025-02-27 20:38:51 -0300

    feature: initial IntegrationTest

---
+++ src/test/java/dev/valente/desafiovagaitau/controller/TransactionControllerTestIT.java
@@ -0,0 +1,54 @@
+package dev.valente.desafiovagaitau.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.valente.desafiovagaitau.config.RestAssuredConfig;
+import dev.valente.desafiovagaitau.dto.TransactionDTO;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RestAssuredConfig.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TransactionControllerTestIT {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private RequestSpecification requestSpecification;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.requestSpecification = requestSpecification;
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Should return created 201 when transaction was save successfully")
+    void salvarTransacao_ShouldReturnCreated_whenSuccessfully() throws JsonProcessingException {
+
+        TransactionDTO dto = TransactionDTO.builder()
+                .valor(BigDecimal.valueOf(50.00))
+                .dataHora(OffsetDateTime.now()).build();
+
+        System.out.println(dto.toString());
+
+        var json = objectMapper.writeValueAsString(dto);
+        System.out.println(json);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(json)
+                .post("/transacao")
+                .then()
+                .statusCode(201)
+                .log().all();
+    }
+}
