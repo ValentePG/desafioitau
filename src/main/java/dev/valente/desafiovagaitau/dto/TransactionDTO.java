1585f507cf77cbfc800ac56ca9e89b7aff719a8a
Valente <bielvhv3@gmail.com>
2025-02-28 20:30:16 -0300

    feature: Validation to TransactionDTO

--- src/main/java/dev/valente/desafiovagaitau/dto/TransactionDTO.java
+++ src/main/java/dev/valente/desafiovagaitau/dto/TransactionDTO.java
@@ -1,10 +1,12 @@
 package dev.valente.desafiovagaitau.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Builder
-public record TransactionDTO(BigDecimal valor, OffsetDateTime dataHora) {
+public record TransactionDTO(@NotNull @PositiveOrZero BigDecimal valor, @NotNull OffsetDateTime dataHora) {
 }
