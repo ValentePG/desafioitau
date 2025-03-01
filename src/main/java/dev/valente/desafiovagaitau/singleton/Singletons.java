09f4e43b4a14d917ae7b878d265606161a3a4ed7
Valente <bielvhv3@gmail.com>
2025-03-01 19:59:29 -0300

    feat: Renomeando nome da classe e adicionando uma fila concorrente para salvar as transações

--- src/main/java/dev/valente/desafiovagaitau/singleton/Singletons.java
+++ src/main/java/dev/valente/desafiovagaitau/singleton/Singletons.java
@@ -1,24 +1,29 @@
 package dev.valente.desafiovagaitau.singleton;
 
+import dev.valente.desafiovagaitau.dto.TransactionDTO;
 import org.springframework.stereotype.Component;
 
 import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Component
-public class SingletonHashMap {
+public class Singletons {
 
-    private static SingletonHashMap instance;
+    private static Singletons instance;
     private final ConcurrentHashMap<Long, DoubleSummaryStatistics> map;
+    private final Queue<TransactionDTO> queue;
 
-    private SingletonHashMap() {
+    private Singletons() {
+        queue = new ConcurrentLinkedQueue<>();
         map = new ConcurrentHashMap<>();
         map.put(1L, new DoubleSummaryStatistics());
     }
 
-    public static SingletonHashMap getInstance() {
+    public static Singletons getInstance() {
         if(instance == null){
-            return new SingletonHashMap();
+            return new Singletons();
         }
         return instance;
     }
@@ -26,4 +31,5 @@ public class SingletonHashMap {
     public ConcurrentHashMap<Long, DoubleSummaryStatistics> getMap() {
         return map;
     }
+    public Queue<TransactionDTO> getQueue() { return queue; }
 }
