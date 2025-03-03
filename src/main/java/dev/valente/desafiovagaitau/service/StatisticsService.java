5e2946cda28b2c57af212f4db5c602445e4f9cda
Valente <bielvhv3@gmail.com>
2025-03-03 17:32:23 -0300

    feature: Adicionando regra de negócio em StatisticsService

---
+++ src/main/java/dev/valente/desafiovagaitau/service/StatisticsService.java
@@ -0,0 +1,60 @@
+package dev.valente.desafiovagaitau.service;
+
+import dev.valente.desafiovagaitau.config.Properties;
+import dev.valente.desafiovagaitau.domain.Statistics;
+import dev.valente.desafiovagaitau.domain.Transaction;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Queue;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class StatisticsService {
+
+    private final TransactionService transactionService;
+    private final Properties properties;
+
+    public Statistics getStatistics() {
+
+        var queue = transactionService.getQueue();
+
+        var filteredList = filterList(queue);
+        var summaryStatistics = calculateStatistics(filteredList);
+
+        return createStatistics(summaryStatistics);
+    }
+
+    private List<Transaction> filterList(Queue<Transaction> queue){
+        log.info("Time Window: '{}'", properties.timeWindow());
+        return queue
+                .stream()
+                .filter(l -> Duration.between(l.getDataHora(),
+                        OffsetDateTime.now(ZoneOffset.of("-03:00"))).getSeconds() <= properties.timeWindow())
+                .toList();
+    }
+
+    private DoubleSummaryStatistics calculateStatistics(List<Transaction> transactions) {
+        var summary = new DoubleSummaryStatistics();
+        transactions.forEach(transaction -> summary.accept(transaction.getValor().doubleValue()));
+        return summary;
+    }
+
+    private Statistics createStatistics(DoubleSummaryStatistics statistics) {
+        return Statistics.builder()
+                .sum(statistics.getSum())
+                .min(statistics.getMin())
+                .max(statistics.getMax())
+                .average(statistics.getAverage())
+                .count(statistics.getCount())
+                .build();
+    }
+
+}
