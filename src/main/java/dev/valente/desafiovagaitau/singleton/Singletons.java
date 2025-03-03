334c4eb04d97aff00eb361d58e43816018b1897c
Valente <bielvhv3@gmail.com>
2025-03-03 10:41:58 -0300

    refactor: Separando a classe DoubleSummaryStatistics

--- src/main/java/dev/valente/desafiovagaitau/singleton/Singletons.java
+++ src/main/java/dev/valente/desafiovagaitau/singleton/Singletons.java
@@ -3,33 +3,27 @@ package dev.valente.desafiovagaitau.singleton;
 import dev.valente.desafiovagaitau.dto.TransactionDTO;
 import org.springframework.stereotype.Component;
 
-import java.util.DoubleSummaryStatistics;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Component
 public class Singletons {
 
-    private static Singletons instance;
-    private final ConcurrentHashMap<Long, DoubleSummaryStatistics> map;
+    private static final Singletons INSTANCE = new Singletons();
+
     private final Queue<TransactionDTO> queue;
 
     private Singletons() {
         queue = new ConcurrentLinkedQueue<>();
-        map = new ConcurrentHashMap<>();
-        map.put(1L, new DoubleSummaryStatistics());
+
     }
 
     public static Singletons getInstance() {
-        if(instance == null){
-            return new Singletons();
-        }
-        return instance;
+        return INSTANCE;
     }
 
-    public ConcurrentHashMap<Long, DoubleSummaryStatistics> getMap() {
-        return map;
+
+    public Queue<TransactionDTO> getQueue() {
+        return queue;
     }
-    public Queue<TransactionDTO> getQueue() { return queue; }
 }
