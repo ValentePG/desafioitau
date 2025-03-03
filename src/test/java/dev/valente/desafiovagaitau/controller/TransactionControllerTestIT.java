cded83a1596b5a62be43fd144e82291c8b9b655b
Valente <bielvhv3@gmail.com>
2025-03-03 17:27:24 -0300

    refactor: refatorando testes de integração, adicionando request a partir de um arquivo

--- src/test/java/dev/valente/desafiovagaitau/controller/TransactionControllerTestIT.java
+++ src/test/java/dev/valente/desafiovagaitau/controller/TransactionControllerTestIT.java
@@ -1,28 +1,22 @@
 package dev.valente.desafiovagaitau.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.valente.desafiovagaitau.config.RestAssuredConfig;
-import dev.valente.desafiovagaitau.dto.TransactionDTO;
+import dev.valente.desafiovagaitau.utils.FileUtils;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RestAssuredConfig.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TransactionControllerTestIT {
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private RequestSpecification requestSpecification;
 
     @Autowired
-    private RequestSpecification requestSpecification;
+    private FileUtils fileUtils;
 
     @BeforeEach
     void setUp() {
@@ -32,27 +26,18 @@ class TransactionControllerTestIT {
     @Test
     @Order(1)
     @DisplayName("Should return created 201 when transaction was save successfully")
-    void salvarTransacao_ShouldReturnCreated_whenSuccessfully() throws JsonProcessingException {
+    void saveTransaction_ShouldReturnCreated_whenSuccessfully() {
 
-        TransactionDTO dto = TransactionDTO.builder()
-                .valor(BigDecimal.valueOf(50.00))
-                .dataHora(OffsetDateTime.now(ZoneOffset.of("-03:00"))).build();
+        var request = fileUtils.readFile("requests/post_savetransaction_201.json");
 
-        System.out.println(dto.dataHora());
-
-        var json = objectMapper.writeValueAsString(dto);
-        var count = 10;
-        while (count > 0){
-            RestAssured.given()
-                    .contentType("application/json")
-                    .accept("application/json")
-                    .body(json)
-                    .post("/transacao")
-                    .then()
-                    .statusCode(201)
-                    .log().all();
-            count--;
-        }
+        RestAssured.given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(request)
+                .post("/transacao")
+                .then()
+                .statusCode(201)
+                .log().all();
 
     }
 }
