f4cd52ecf430b3cc3c8015305a3aa9bb8dcd3c89
Valente <bielvhv3@gmail.com>
2025-03-03 17:26:35 -0300

    feature: Adicionando classes de testes unitários

---
+++ src/test/java/dev/valente/desafiovagaitau/service/StatisticsServiceTest.java
@@ -0,0 +1,14 @@
+package dev.valente.desafiovagaitau.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StatisticsServiceTest {
+
+}
