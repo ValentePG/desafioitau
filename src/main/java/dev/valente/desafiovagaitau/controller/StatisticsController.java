3d20ccb7d464b0e4930b6486bb6d913fa0f2147b
Valente <bielvhv3@gmail.com>
2025-03-03 17:31:57 -0300

    feature: Adicionando operações principais do controlador

--- src/main/java/dev/valente/desafiovagaitau/controller/StatisticsController.java
+++ src/main/java/dev/valente/desafiovagaitau/controller/StatisticsController.java
@@ -1,6 +1,8 @@
 package dev.valente.desafiovagaitau.controller;
 
 import dev.valente.desafiovagaitau.dto.StatisticsResponseDTO;
+import dev.valente.desafiovagaitau.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("estatistica")
+@RequiredArgsConstructor
 public class StatisticsController {
 
+    private final StatisticsService statisticsService;
+
     @GetMapping
-    public ResponseEntity<StatisticsResponseDTO> mostrarEstatisticas() {
-        return null;
+    public ResponseEntity<StatisticsResponseDTO> showStatistics() {
+
+        var statistics = statisticsService.getStatistics();
+        var statisticsResponseDTO = StatisticsResponseDTO.builder()
+                .sum(statistics.getSum())
+                .max(statistics.getMax())
+                .min(statistics.getMin())
+                .average(statistics.getAverage())
+                .count(statistics.getCount())
+                .build();
+
+        return ResponseEntity.ok(statisticsResponseDTO);
     }
 }
