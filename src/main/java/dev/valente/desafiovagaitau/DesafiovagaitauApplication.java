5e646ce60e5a82e92f088804d9f822b186601dbc
Valente <bielvhv3@gmail.com>
2025-03-03 17:34:41 -0300

    feature: Habilitando ConfigurationProperties

--- src/main/java/dev/valente/desafiovagaitau/DesafiovagaitauApplication.java
+++ src/main/java/dev/valente/desafiovagaitau/DesafiovagaitauApplication.java
@@ -1,9 +1,12 @@
 package dev.valente.desafiovagaitau;
 
+import dev.valente.desafiovagaitau.config.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Properties.class)
 public class DesafiovagaitauApplication {
 
 	public static void main(String[] args) {
