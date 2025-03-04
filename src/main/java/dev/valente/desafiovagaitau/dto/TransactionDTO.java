9c872b315b2a26952aa35752baa0e5ccf347387d
Valente <bielvhv3@gmail.com>
2025-03-04 19:37:04 -0300

    feature: Adicionando mensagem para validações

--- src/main/java/dev/valente/desafiovagaitau/dto/TransactionDTO.java
+++ src/main/java/dev/valente/desafiovagaitau/dto/TransactionDTO.java
@@ -8,5 +8,9 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Builder
-public record TransactionDTO(@NotNull @PositiveOrZero BigDecimal valor, @NotNull OffsetDateTime dataHora) {
+public record TransactionDTO(@NotNull(message = "O valor não pode estar vazio")
+                             @PositiveOrZero(message = "O Valor da transação deve ser positivo ou igual a 0")
+                             BigDecimal valor,
+                             @NotNull(message = "O campo dataHora não deve estar vazio")
+                             OffsetDateTime dataHora) {
 }
