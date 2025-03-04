416b99fb2c11f976da6a49b95186be758c769a65
Valente <bielvhv3@gmail.com>
2025-03-04 19:38:59 -0300

    feature: Adicionando exception handler e Formato padrão para resposta para requisições inválidas.

---
+++ src/main/java/dev/valente/desafiovagaitau/exception/ApiError.java
@@ -0,0 +1,17 @@
+package dev.valente.desafiovagaitau.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+
+@Builder
+@Getter
+public class ApiError {
+
+    private OffsetDateTime timestamp;
+    private int statusCode;
+    private String error;
+    private String message;
+    private String path;
+}
---
+++ src/main/java/dev/valente/desafiovagaitau/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package dev.valente.desafiovagaitau.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                          HttpServletRequest request){
+        var defaultMessage = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+
+        var apiError = ApiError.builder()
+                .timestamp(OffsetDateTime.now())
+                .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .error(HttpStatus.UNPROCESSABLE_ENTITY.getReasonPhrase())
+                .message(defaultMessage)
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(apiError);
+    }
+}
