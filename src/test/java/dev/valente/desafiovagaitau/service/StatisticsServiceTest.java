15709d0fd3248db23ee4400174226015b7281acf
Valente <bielvhv3@gmail.com>
2025-03-04 20:47:03 -0300

    feature: Adicionando test unitário (incompleto)

--- src/test/java/dev/valente/desafiovagaitau/service/StatisticsServiceTest.java
+++ src/test/java/dev/valente/desafiovagaitau/service/StatisticsServiceTest.java
@@ -1,14 +1,67 @@
 package dev.valente.desafiovagaitau.service;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import dev.valente.desafiovagaitau.config.Properties;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+
+import static dev.valente.desafiovagaitau.utils.StatisticsUtil.EMPTY_QUEUE;
+import static dev.valente.desafiovagaitau.utils.StatisticsUtil.QUEUE;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StatisticsServiceTest {
+
+    @InjectMocks
+    private StatisticsService statisticsService;
+
+    @Mock
+    private TransactionService transactionService;
+
+    @Mock
+    private Properties properties;
+
+    @Test
+    @Order(1)
+    @DisplayName("Should return statistics successfully")
+    void getStatistics_shouldReturnStatistics_WhenSuccessful() {
+        BDDMockito.when(transactionService.getQueue()).thenReturn(QUEUE);
+        BDDMockito.when(properties.timeWindow()).thenReturn(60);
+
+        var sut = statisticsService.getStatistics();
+
+        Assertions.assertThat(sut)
+                .hasFieldOrPropertyWithValue("sum", 150.0)
+                .hasFieldOrPropertyWithValue("average", new BigDecimal("75.00"))
+                .hasFieldOrPropertyWithValue("min", 50.0)
+                .hasFieldOrPropertyWithValue("max", 100.0)
+                .hasFieldOrPropertyWithValue("count", 2L)
+                .hasNoNullFieldsOrProperties();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Should return statistics successfully")
+    void getStatistics_shouldReturnStatisticsWithValue0_WhenSuccessful() {
+        BDDMockito.when(transactionService.getQueue()).thenReturn(EMPTY_QUEUE);
+        BDDMockito.when(properties.timeWindow()).thenReturn(60);
+
+        var sut = statisticsService.getStatistics();
+
+        Assertions.assertThat(sut)
+                .hasFieldOrPropertyWithValue("sum", 0.0)
+                .hasFieldOrPropertyWithValue("average", new BigDecimal("0.00"))
+                .hasFieldOrPropertyWithValue("min", 0.0)
+                .hasFieldOrPropertyWithValue("max", 0.0)
+                .hasFieldOrPropertyWithValue("count", 0L)
+                .hasNoNullFieldsOrProperties();
+    }
 
 }
