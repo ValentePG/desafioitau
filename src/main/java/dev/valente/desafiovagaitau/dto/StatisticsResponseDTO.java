604454e952fc8a022e7260e986bd2355de9f829d
Valente <bielvhv3@gmail.com>
2025-03-05 20:35:06 -0300

    refactor: mudando average para double

--- src/main/java/dev/valente/desafiovagaitau/dto/StatisticsResponseDTO.java
+++ src/main/java/dev/valente/desafiovagaitau/dto/StatisticsResponseDTO.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 @Builder
-public record StatisticsResponseDTO(double sum, BigDecimal average, double min, double max, long count) {
+public record StatisticsResponseDTO(double sum, double average, double min, double max, long count) {
 }
